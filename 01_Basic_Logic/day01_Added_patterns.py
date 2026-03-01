7f1659824b46d5686dd1c907760a060da34ef508
Rahul Kumar <rkrahul9953@gmail.com>
2026-03-01 15:38:31 +0530

    Day 01: Added triangle, pyramid and inverted patterns

---
+++ 01_Basic_Logic/day01_Added_patterns.py
@@ -0,0 +1,44 @@
+def triangle(n):
+    for i in range(1, n):
+        for j in range(i):
+            print("*", end="")
+        print()
+        
+def pyramid(n):
+    for i in range(1, n+1):
+        for j in range(n - i):
+            print(" ", end = "")
+        for j in range(2 * i - 1):
+            print("*", end = "")
+        print()
+        
+def inverted(n):
+    for i in range(1, n+1):
+        for j in range(i-1):
+            print(" ", end="")
+        for j in range(2*(n-i)+1):
+            print("*", end="")
+        print()
+        
+while True:
+    print("Menu")
+    print("1.Triangle")
+    print("2.Pyramid")
+    print("3.Inverted Pyramid")
+    print("4.Exit")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        n = int(input("No. of Rows: "))
+        triangle(n)
+    elif choice == 2:
+         n = int(input("No. of Rows: "))
+         pyramid(n)
+         
+    elif choice == 3:
+              n = int(input("No. of Rows: "))
+              inverted(n)
+    elif choice == 4:
+        print("exiting")
+        break
+    else:
+        print("Invalid choice")
