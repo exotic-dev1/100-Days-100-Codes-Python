fe79b55d5f5a76e6d65da8b40a176f6b64132992
Rahul Kumar <rkrahul9953@gmail.com>
2026-04-18 21:34:50 +0530

    Day49: Check balanced binary tree

---
+++ 07_Prefix_Sum_and_Hashmap/Day49_Check_Balanced_Binary_Tree.py
@@ -0,0 +1,39 @@
+"""
+Day49 :- Check Balanced Binary Tree
+Difficulty :- Medium
+Concepts :- recursion, trees, optimization
+"""
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def isBalanced(root):
+    def check_height(node):
+        if not node:
+            return 0
+        
+        left_height = check_height(node.left)
+        if left_height == -1:
+            return -1
+        
+        right_height = check_height(node.right)
+        if right_height == -1:
+            return -1
+        
+        if abs(left_height - right_height) > 1:
+            return -1
+        
+        return 1 + max(left_height, right_height)
+    
+    return check_height(root) != -1
+    
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+print(isBalanced(root))
